2f6c5d157277d31c7e92f68fef9e10da242ead3f
Alexei Andreiev <darkwasabj@gmail.com>
2022-09-16 20:57:42 +0300

    Use contructor promotion

--- src/HorizonAccess.php
+++ src/HorizonAccess.php
@@ -6,14 +6,12 @@ use Illuminate\Support\Facades\Config;
 
 class HorizonAccess
 {
-    private GateProvider $gateProvider;
-
     /**
      * @param GateProvider $gateProvider
      */
-    public function __construct(GateProvider $gateProvider)
-    {
-        $this->gateProvider = $gateProvider;
+    public function __construct(
+        private GateProvider $gateProvider
+    ) {
     }
 
     public function defineGate(): void
