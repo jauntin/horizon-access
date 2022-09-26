bd4677e30c918130f35b34ccfadedafc222e4ae3
Dasha Syvyuk <dashasyvyuk@Dashas-MacBook-Pro.local>
2022-09-26 15:24:23 +0300

    fix response

--- src/Http/Middleware/RedirectToSocialIfNotAuthenticated.php
+++ src/Http/Middleware/RedirectToSocialIfNotAuthenticated.php
@@ -3,6 +3,7 @@
 namespace Jauntin\HorizonAccess\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -16,9 +17,9 @@ class RedirectToSocialIfNotAuthenticated
      * @param Request $request
      * @param Closure $next
      *
-     * @return Response|RedirectResponse
+     * @return Response|RedirectResponse|JsonResponse
      */
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         if (
             HorizonAccess::enabled() &&
